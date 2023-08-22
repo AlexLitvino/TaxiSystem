97bcd57aecfec5f63f3a38f4c8f6acb5db81bc71
AlexLitvino <litvinov.aleks@gmail.com>
2023-08-22 23:35:25 +0300

    map generator updated to min-max coordinates; re-generated maps

--- maps/map_generator.py
+++ maps/map_generator.py
@@ -3,8 +3,8 @@ import random
 
 
 # TODO: faker could be used for real-like addresses
-def generate_map(city: str, address_count):
-    map_ = {f"Address{i}": random.randint(1000, 9999) for i in range(1, address_count)}
+def generate_map(city: str, address_count, min_coordinate=0, max_coordinate=35):
+    map_ = {f"Address{i}": random.randint(min_coordinate, max_coordinate) for i in range(1, address_count)}
     map_file_name = f"{city}.json"
     with open(map_file_name, 'w') as f:
         json.dump(map_, f, indent=4)
